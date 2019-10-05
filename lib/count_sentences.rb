4937174cb12153b14bf1bac19678da0c6d6b7c58
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-05 17:33:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,8 @@ self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+ sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if{|sentence| sentence.empty?}
+    sentence_array.length
   end
 end
